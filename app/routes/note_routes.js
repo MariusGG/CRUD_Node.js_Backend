a94b2e094731b02c7557bcbcd57932cf21f61565
Marius Guei <marius.g.guei@gmail.com>
2019-02-08 20:32:19 +0000

    can now post notes into database

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,15 @@
 module.exports = function(app, db) {
 
-app.post('/notes', (request, response))  
+app.post('/notes', (request, response) => {
+    // this will create a note here
+    const note = { text: request.body.body, title: request.body.title};
+    db.collection('notes').insert(note, (error, result) => {
+      if (error) {
+        response.send({ 'error': 'An error has occured' });
+      } else {
+        response.send(result.ops[0])
+      }
+    });
+});
+
 };
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
+const express      = require('express');
+const MongoClient  = require('mongodb').MongoClient;
+const bodyParser   = require('body-parser');
+const db           = require('./config/db')
 
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log("I'm listening on " + port)
-  console.log("hello MariusGG")
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+MongoClient.connect(db.url, ( error, database) => {
+  if (error) return console.log(error)
+
+  require('./app/routes')(app, database);
+
+  app.listen(port, () => {
+    console.log("I'm listening on " + port)
+
+  })
 })
