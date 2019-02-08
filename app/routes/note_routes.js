2f0c3795e214c05cf7e2383aa5ac66ff9816e005
Marius Guei <marius.g.guei@gmail.com>
2019-02-08 21:30:02 +0000

    backend CRUD done for a simple notes app

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,15 +1,69 @@
+var ObjectID = require('Mongodb').ObjectID
+
 module.exports = function(app, db) {
 
-app.post('/notes', (request, response) => {
-    // this will create a note here
-    const note = { text: request.body.body, title: request.body.title};
-    db.collection('notes').insert(note, (error, result) => {
-      if (error) {
-        response.send({ 'error': 'An error has occured' });
-      } else {
-        response.send(result.ops[0])
-      }
+  app.get('/notes/:id', (request, response) => {
+      // this will get an existing note from the database
+      // this is making a request from the database
+      const id = request.params.id
+      // I've made a request for an id which gets stored into 'id'
+      // now the 'id' is passed as a new object id to get passed into mongo
+      const informations = { '_id': new ObjectID(id) };
+
+      db.collection('notes').findOne(informations, (error, item) => {
+        if (error) {
+          response.send({ 'error': 'An error has occured' });
+        } else {
+          response.send(item)
+        }
+      });
+  });
+
+  app.delete('/notes/:id', (request, response) => {
+      // this will get an existing note from the database
+      // this is making a request from the database
+      const id = request.params.id
+      // I've made a request for an id which gets stored into 'id'
+      // now the 'id' is passed as a new object id to get passed into mongo
+      const informations = { '_id': new ObjectID(id) };
+
+      db.collection('notes').remove(informations, (error, item) => {
+        if (error) {
+          response.send({ 'error': 'An error has occured' });
+        } else {
+          response.send("Note " + id + " was deleted!")
+        }
+      });
+  });
+  app.put('/notes/:id', (request, response) => {
+      // this will get an existing note from the database
+      // this is making a request from the database
+      const id = request.params.id
+      // I've made a request for an id which gets stored into 'id'
+      // now the 'id' is passed as a new object id to get passed into mongo
+      const informations = { '_id': new ObjectID(id) };
+      // just like post you can edit the title and body
+      const note = { text: request.body.body, title: request.body.title};
+      db.collection('notes').update(informations, note, (error, item) => {
+        if (error) {
+          response.send({ 'error': 'An error has occured' });
+        } else {
+          response.send(item)
+        }
+      });
+  });
+
+  app.post('/notes', (request, response) => {
+      // this will create a note here
+        const note = { text: request.body.body, title: request.body.title};
+        db.collection('notes').insert(note, (error, result) => {
+          if (error) {
+            response.send({ 'error': 'An error has occured' });
+          } else {
+            response.send(result.ops[0])
+          }
+        });
     });
-});
+
 
 };
